7faed37abb31e0299b23ee3219e8ec2a158627c8
Pavlo Tymoshenko <ptymoshenko@users.noreply.github.com>
2026-05-14 17:30:00 +0200

    Updates from Ship Studio

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,13 +1,31 @@
 "use client";
 
 import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import LocomotiveScroll from "locomotive-scroll";
 import "locomotive-scroll/locomotive-scroll.css";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function SmoothScroll() {
   useEffect(() => {
-    const scroll = new LocomotiveScroll();
-    return () => scroll.destroy();
+    const scroll = new LocomotiveScroll({
+      initCustomTicker: (onRender) => gsap.ticker.add(onRender),
+      destroyCustomTicker: (onRender) => gsap.ticker.remove(onRender),
+    });
+
+    const lenis = scroll.lenisInstance;
+    if (lenis) {
+      lenis.on("scroll", ScrollTrigger.update);
+    }
+
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      if (lenis) lenis.off("scroll", ScrollTrigger.update);
+      scroll.destroy();
+    };
   }, []);
 
   return null;
